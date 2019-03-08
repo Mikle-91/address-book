5cb391f99e06ee976e959e962fdb26ca1a36e713
mtopchiev <47459395+mtopchiev@users.noreply.github.com>
2019-03-09 01:38:38 +0300

    create fixture for unittest-add group
    выделили фикстуру в отдельный класс

--- test_add_group.py
+++ test_add_group.py
@@ -1,64 +1,20 @@
 # # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
+from application import Application
 import time, unittest
 from group import Group
 
 class AppDynamicsJob(unittest.TestCase):
     def setUp(self):
-        self.driver = WebDriver()
-        self.driver.implicitly_wait(30)
-        self.base_url = "http://localhost/addressbook/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
+        self.app = Application()
     
     def test_app_dynamics_job(self):
-        self.login(username="admin", password="secret")
-        self.create_new_groupe(Group(name="uyt", header="headqwe", footer="footasd"))
-        self.logout()
+        self.app.login(username="admin", password="secret")
+        self.app.create_new_groupe(Group(name="uyt", header="headqwe", footer="footasd"))
+        self.app.logout()
 
-    def logout(self):
-        driver = self.driver
-        driver.find_element_by_link_text("groups").click()
-        driver.find_element_by_link_text("Logout").click()
-
-    def create_new_groupe(self, Group):
-        driver = self.driver
-        self.open_groups_page()
-        # init groupe creation
-        driver.find_element_by_name("new").click()
-        # fill group form
-        driver.find_element_by_name("group_name").click()
-        driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys(Group.name)
-        driver.find_element_by_name("group_header").click()
-        driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys(Group.header)
-        driver.find_element_by_name("group_footer").click()
-        driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys(Group.footer)
-        # submit group
-        driver.find_element_by_name("submit").click()
-
-    def open_groups_page(self):
-        driver = self.driver
-        driver.find_element_by_link_text("groups").click()
-
-    def login(self, username, password):
-        driver = self.driver
-        self.open_home_page()
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys(username)
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys(password)
-        driver.find_element_by_id("LoginForm").submit()
-
-    def open_home_page(self):
-        driver = self.driver
-        driver.get("http://localhost/addressbook/")
 
     def tearDown(self):
-        self.driver.quit() #закрытие браузера
+        self.app.destroy()
 
 if __name__ == "__main__":
     unittest.main()
