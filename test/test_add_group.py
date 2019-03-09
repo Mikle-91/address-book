7ea69f5d0ec06696b4e050a1d46869741ce2b0dc
mtopchiev <47459395+mtopchiev@users.noreply.github.com>
2019-03-09 14:26:05 +0300

    Create packages for different areas of responsibility
    Packages: test, model, fixture

---
+++ test/test_add_group.py
@@ -0,0 +1,17 @@
+# # -*- coding: utf-8 -*-
+import pytest
+from model.group import Group
+from fixture.application import Application
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_app_dynamics_job(app):
+    app.login(username="admin", password="secret")
+    app.create_new_groupe(Group(name="uyt", header="headqwe", footer="footasd"))
+    app.logout()
