3de13fe5f9d2dc90845884fda596e9009d2e01f6
mtopchiev <47459395+mtopchiev@users.noreply.github.com>
2019-03-14 23:51:45 +0300

    create folder data for testdata

--- conftest.py
+++ conftest.py
@@ -26,8 +26,9 @@ def app(request):
     global target
     browser = request.config.getoption("--browser")  # передаем опц значение в конструктор application
     if target is None:
-        with open (request.config.getoption("--target")) as config_file:
-            target=json.load(config_file)
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))  # путь к файлу target.json
+        with open (config_file) as f:
+            target=json.load(f)
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=target['baseUrl'])   # если фикстура сломалась, запускаем новую
         #web_config = load_config(request.config.getoption("--target"))['web']
@@ -35,18 +36,17 @@ def app(request):
     return fixture
 
 
-# @pytest.fixture(scope="session")
-# def db(request):
-#     db_config = load_config(request.config.getoption("--target"))['db']
-#     dbfixture = DbFixture(host=db_config['host'], name=db_config['name'], user=db_config['user'], password=db_config['password'])
-#     def fin():
-#         dbfixture.destroy()
-#     request.addfinalizer(fin)
-#     return dbfixture
+@pytest.fixture(scope="session")
+def db(request):
+    db_config = load_config(request.config.getoption("--target"))['db']
+    dbfixture = DbFixture(host=db_config['host'], name=db_config['name'], user=db_config['user'], password=db_config['password'])
+    def fin():
+        dbfixture.destroy()
+    request.addfinalizer(fin)
+    return dbfixture
 
 
 @pytest.fixture(scope="session", autouse=True)  #scope session чтобы логаут был после прохождения всех тестов, autouse чтобы автоматом завершил сессию
-#@pytest.fixture(scope="session", autouse=True)
 def stop(request):
     def fin():
         fixture.session.ensure_logout()
@@ -57,44 +57,3 @@ def stop(request):
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox") #store значит сохранить значение параметра
     parser.addoption("--target", action="store", default="target.json")
-    #parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
-
-
-
-
-
-
-
-
-
-
-
-
-
-# @pytest.fixture #без пяти меннут переделаная на половину
-# def app(request):
-#     global fixture
-#     if fixture is None:
-#         browser = request.config.getoption("--browser")
-#         web_config = load_config(request.config.getoption("--target"))['web'] # до сюда
-#         fixture = Application(browser=browser)
-#         fixture.session.login(username="admin", password="secret")
-#     else:
-#         if not fixture.is_valid():
-#             fixture = Application()
-#             fixture.session.login(username="admin", password="secret")
-#     return fixture
-
-
-
-
-
-# @pytest.fixture(scope="session")  #предыдущая версия фикстуры
-# def app(request):
-#     fixture = Application()
-#     fixture.session.login(username="admin", password="secret")
-#     def fin():
-#         fixture.session.logout()
-#         fixture.destroy()
-#     request.addfinalizer(fin)
-#     return fixture
