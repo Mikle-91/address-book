12d496109e89ca12a8c4d814fe617de7c19c62fb
mtopchiev <47459395+mtopchiev@users.noreply.github.com>
2019-03-15 15:31:34 +0300

    add test_db_matches_ui

--- conftest.py
+++ conftest.py
@@ -11,7 +11,7 @@ fixture = None
 target =None
 
 
-def load_config(file):
+def load_config(file): # выполняется загрузка конфигурации
     global target
     if target is None:
         config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
@@ -25,20 +25,16 @@ def app(request):
     global fixture
     global target
     browser = request.config.getoption("--browser")  # передаем опц значение в конструктор application
-    if target is None:
-        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))  # путь к файлу target.json
-        with open (config_file) as f:
-            target=json.load(f)
+    web_config = load_config(request.config.getoption("--target"))['web'] #загрузка конфигурации фикстуры из файла
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, base_url=target['baseUrl'])   # если фикстура сломалась, запускаем новую
-        #web_config = load_config(request.config.getoption("--target"))['web']
-    fixture.session.ensure_login(username=target['username'], password=target['password'])
+        fixture = Application(browser=browser, base_url=web_config['baseUrl'])   # если фикстура сломалась, запускаем новую
+    fixture.session.ensure_login(username=web_config['username'], password=web_config['password'])
     return fixture
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="session") #фикстура для тестов БД без запуска браузера
 def db(request):
-    db_config = load_config(request.config.getoption("--target"))['db']
+    db_config = load_config(request.config.getoption("--target"))['db'] #загрузка конфигурации фикстуры из файла
     dbfixture = DbFixture(host=db_config['host'], name=db_config['name'], user=db_config['user'], password=db_config['password'])
     def fin():
         dbfixture.destroy()
@@ -73,5 +69,5 @@ def load_from_module(module):
 
 def load_from_json(file):
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f: #открываем файл с данными в json
-        return jsonpickle.decode(f.read())          #читаем из него данные и перекодируем  
+        return jsonpickle.decode(f.read())          #читаем из него данные и перекодируем
 
--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -1,8 +1,10 @@
 from model.group import Group
-
+from timeit import timeit
 
 def test_group_list(app, db):
     ui_list = app.group.get_group_list()   #список загруженный через ui
-    db_list = db.get_group_list()   #список загруженный из db
+    def clean(group): #clean убирает пробелы перед сравнением групп
+        return Group(id=group.id, name=group.name.strip())
+    db_list = map(clean, db.get_group_list())   #список загруженный из db
     assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max) #сравнение списков
 
