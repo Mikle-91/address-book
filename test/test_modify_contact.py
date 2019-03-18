baeb8f4e3a8cd68de0c0c2e25b9d95aa1be30fd5
mtopchiev <47459395+mtopchiev@users.noreply.github.com>
2019-03-18 18:23:42 +0300

    add more test data to test_modify_contact

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,23 +2,29 @@
 from model.contact import Contact
 from random import randrange
 
-def test_modify_contact_firstname(app, json_modcontact, db): #альтернативный вариант data_contacts (вместо json_contact)
+def test_modify_contact_firstname(app, db): #изменяем данные рандомной группы
     if app.contact.count() == 0:  # проверка наличия групп перед изменением
-        app.contact.create(
-            # Contact(firstname="without note", middlename="pupkin", lastname="last", nickname="supernick", photo=None,
-            #         title="sometitle", company="space", address="zeleka", homephone="5floar", mobilephone="9876", workphone="456",
-            #         fax="45", email="65@qwe.ru", email2="4", email3="45", bday="", bmonth="February", byear="1992",
-            #         aday="30", amonth="January", ayear="1980", new_group=None, address2="someadr 78", secondaryphone="000",
-            #         notes="important notice"))
-             Contact(firstname="NewName", lastname="last"))
-    #old_contacts = app.contact.get_contact_list()
-    old_contacts = db.get_contact_list()
-    contact = Contact(firstname="NewName", lastname="last")  # вместо json
+        app.contact.create(     #если нет ни одной группы, то создаем новую
+            Contact(firstname="without note", middlename="pupkin", lastname="last", nickname="supernick", photo=None,
+                    title="sometitle", company="space", address="zeleka", homephone="5floar", mobilephone="9876", workphone="456",
+                    fax="45", email="65@qwe.ru", email2="4", email3="45", bday="", bmonth="February", byear="1992",
+                    aday="30", amonth="January", ayear="1980", new_group=None, address2="someadr 78", secondaryphone="000",
+                    notes="important notice"))
+
+    #old_contacts = app.contact.get_contact_list() #этот вариант берет список групп из UI
+    old_contacts = db.get_contact_list()    #этот вариант берет список групп из БД
+    #создаем переменную с новыми данными для редактируемой группы
+    contact = Contact(firstname="Ivan", middlename="Pupkin", lastname="Petrovich", nickname="testnickname", photo=None,
+            title="sometitle", company="Gazprom", address="759 Lenin st.", homephone="(987)4132", mobilephone="9123876",
+            workphone="456123",
+            fax="4 5321", email="65@qwe.ru", email2="4987zd@gmail.com", email3="asdfg@gmail.com", bday="8", bmonth="March", byear="1917",
+            aday="30", amonth="January", ayear="1980", new_group=None, address2="someadr", secondaryphone="49577884",
+            notes="nothing")
     index = randrange(len(old_contacts))  # рандомно выбираем какую группу изменить
-    contact.id = old_contacts[index].id
-    app.contact.modify_contact_by_index(index, contact)
+    contact.id = old_contacts[index].id #запомнили id выбранной группы
+    app.contact.modify_contact_by_index(index, contact) #изменяем выбранную группу
     #new_contacts = app.contact.get_contact_list()
-    new_contacts = db.get_contact_list()
+    new_contacts = db.get_contact_list() #берем обновленный список групп из БД
     assert len(old_contacts)  == len(new_contacts) #сравнение количества контактов после изменения группы
     old_contacts[index] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max) # проверка содержания контактов
--- test/test_phones.py
+++ test/test_phones.py
@@ -2,7 +2,7 @@ import re
 from model.contact import Contact
 
 def test_phones_on_home_page(app):
-    
+
 
     contact_from_home_page = app.contact.get_contact_list()[0]  # новая проверка учитывающая незапаолненные номера
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
